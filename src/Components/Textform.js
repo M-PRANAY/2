1a07d87f4f41766318e4ad2474a231b78b7e1981
Pranay <pranaykumarmolleti@gmail.com>
2023-05-30 13:16:44 +0530

    V-7

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function Textform(props) {
+  const [text,setText] = useState('Enter text here');
+  
+  const handleUpClick = () =>{
+    console.log("uppercase button clicked");
+    setText(text.toUpperCase());
+
+  }
+
+  const handleOnChange = (event) => {
+    console.log("Onchange");
+    setText(event.target.value);
+  }
+
+  const handleDownClick = () =>{
+    setText(text.toLowerCase());
+  }
+
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">            
-        <textarea className="form-control" id="myBox" rows="3"></textarea>
-        </div>
-        <button className="btn btn-primary">Conver to UpperCase</button>
+    <>
+      <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">            
+      <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="3"></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>Conver to UpperCase</button>
     </div>
+    <div>
+      <button className="btn btn-primary" onClick={handleDownClick}>Lower</button>
+
+    </div>
+  
+    </>
   )
 }
